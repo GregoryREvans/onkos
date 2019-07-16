44f457dc573c2179092c11e40a63feab9f467cad
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-07-16 18:14:59 -0400

    Rerendered segments.

--- onkos/Segments/Segment_I/definition.py
+++ onkos/Segments/Segment_I/definition.py
@@ -221,13 +221,12 @@ for staff in abjad.iterate(score["Global Context"]).components(abjad.Staff):
 # print('Transforming Tuplet Brackets ...')
 # transformer = NoteheadBracketMaker()
 # transformer(score)
-abjad_stylesheet_path = os.path.join(
-    os.environ.get("HOME"), "abjad/docs/source/_stylesheets/abjad.ily"
-)
-current_directory = pathlib.Path(__file__).parent
-stylesheet_path = (current_directory / ".." / ".." / "Build").resolve()
 score_file = abjad.LilyPondFile.new(
-    score, includes=[abjad_stylesheet_path, f"{stylesheet_path}/first_stylesheet.ily"]
+    score,
+    includes=[
+        "/Users/evansdsg2/abjad/docs/source/_stylesheets/abjad.ily",
+        "/Users/evansdsg2/Scores/onkos/onkos/Build/first_stylesheet.ily",
+    ],
 )
 
 abjad.SegmentMaker.comment_measure_numbers(score)
